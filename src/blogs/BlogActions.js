0506f6076d8dbcc93391bb07298a3f6e96f051a7
sanvi24 <sandhyarani813078@gmail.com>
2024-03-28 20:26:20 +0530

    Update key

--- src/blogs/BlogActions.js
+++ src/blogs/BlogActions.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = "fac1827912944b14b4e9c25bd57c4489";
 
 export const getBlogDetails = createAsyncThunk("blog-details", async() => {
     const response = await axios.get(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${API_KEY}`);
